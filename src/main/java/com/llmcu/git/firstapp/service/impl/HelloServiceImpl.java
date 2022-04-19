0b680b5aa2acd4b13b02eed255f6a93961363311
llmcu <382524176@qq.com>
2022-04-19 20:47:42 +0800

    test idea set

--- src/main/java/com/llmcu/git/firstapp/service/impl/HelloServiceImpl.java
+++ src/main/java/com/llmcu/git/firstapp/service/impl/HelloServiceImpl.java
@@ -3,14 +3,19 @@ package com.llmcu.git.firstapp.service.impl;
 import com.llmcu.git.firstapp.service.HelloService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 /**
  * @author llmcu
  */
 @Service
 public class HelloServiceImpl implements HelloService {
 
+
     @Override
     public String sayHello() {
+        new ArrayList<>();
+        System.out.println("Hello World!");
         return "Hello World!!";
     }
 }
